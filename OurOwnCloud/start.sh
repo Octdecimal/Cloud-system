1b4c9456365698b4a4361a231e935a93e3059e6a
Octdecimal <572neet989@gmail.com>
2025-05-16 00:14:15 +0800

    Run backend and frontend services concurrently in start.sh

--- OurOwnCloud/start.sh
+++ OurOwnCloud/start.sh
@@ -6,8 +6,8 @@ echo "Container is running on IP: $CONTAINER_IP"
 
 # Navigate to the backend and run uvicorn
 cd /backend
-uvicorn main:app --host 0.0.0.0 --port 8000 --reload 
+uvicorn main:app --host 0.0.0.0 --port 8000 --reload &
 
-# # Navigate to the frontend and run npm
-# cd /vue
-# npm run dev -- --host 0.0.0.0 --port 5173
+# Navigate to the frontend and run npm
+cd /vue
+npm run dev -- --host 0.0.0.0 --port 5173
