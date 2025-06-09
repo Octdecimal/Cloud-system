6aa390d3180862e24ad566658ed70eb62eda2507
Octdecimal <572neet989@gmail.com>
2025-06-09 15:10:38 +0800

    Remove sensitive credentials from LineBotApi and WebhookHandler initialization

--- linebot/範例程式/5-1/linebotTest.py
+++ linebot/範例程式/5-1/linebotTest.py
@@ -6,8 +6,8 @@ from linebot import  LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
-line_bot_api = LineBotApi('r5g/c+48uHsULz72ndBEiFoLXz1cePj8hW1TMGi5fsy+PhRMk03Fm5XkYp1a8OBLrceHCUsWbmNKbk7la/6PRC4Kd0vB/DhjYAAZ0HWaE9Obtw/IIlN0sbKwAi1wg0kYW6T8SCd91Qw5suk/I3V7FgdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('36582c2446eb6708c1f48a559c5ecd96')
+line_bot_api = LineBotApi('')
+handler = WebhookHandler('')
 
 @app.route("/callback", methods=['POST'])
 def callback():
